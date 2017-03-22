1bc794b6cb6076a1b95c3c1c7e58860f12477dc8
nriaziat <nriaziat@gmail.com>
2017-03-22 17:23:59 -0400

    minor text fixes

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+from BeautifulSoup import BeautifulSoup as bs
 from flask import Flask
 from flask_ask import Ask, statement, session, question
+import urlparse
+from urllib2 import urlopen
+from urllib import urlretrieve
+import os
+import sys
 
 app = Flask(__name__)
 ask = Ask(app, '/')
+sess = requests.Session()
 
 baseURL = "http://www.housing.purdue.edu/Menus/"
 courtsURL = {"Earhart": "ERHT", "Ford": "FORD", "Hillenbrand":"HILL", "Wiley":"WILY", "Windosor":"WIND"}
@@ -18,9 +25,10 @@ def start_skill():
 @ask.intent("GET_MEAL", mapping={'court': 'Court'}, mapping={'meal': 'Meal'})
 def function(court, meal):
     numMeals = mealNum(court)
-    URL = baseURL + courtsURL(court)
-    html = sess.get(URL)
-
+    url = baseURL + courtsURL(court)
+    soup = bs(urlopen(url))
+    parsed = list(urlparse.urlparse(url))
+    
     return statement('%s is serving %s for %s', %(court, serving, meal))
 
 if __name__ == '__main__':
